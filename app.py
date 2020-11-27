29250f50ee1e10ba5539f7265071be114a3154e0
SL-Lee <45960387+SL-Lee@users.noreply.github.com>
2020-11-27 22:59:51 +0800

    Implement basic API infrastructure

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import (
     abort,
+    Blueprint,
     flash,
     Flask,
     make_response,
@@ -15,16 +16,29 @@ from flask_login import (
     LoginManager,
     logout_user,
 )
-from models import db
+from flask_restx import Api, reqparse, Resource
+from client_models import (
+    client_db,
+    Product,
+    Review,
+    User,
+)
+from sqlalchemy import exc
+import base64
+import json
+import pickle
 import os
 
 app = Flask(__name__)
-
 app.secret_key = os.urandom(16)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///client_db.sqlite3"
 
-db.init_app(app)
+blueprint = Blueprint("api", __name__, url_prefix="/api")
+api = Api(blueprint, doc="/doc/")
+app.register_blueprint(blueprint)
+
+client_db.init_app(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -59,5 +73,120 @@ def logout():
     return redirect(url_for("index"))
 
 
+# API routes
+@api.route("/query")
+class Query(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("model", required=True, type=str, location="form")
+    parser.add_argument("filter", required=True, type=str, location="form")
+
+    @api.expect(parser)
+    @api.response(200, "Success")
+    @api.response(400, "Invalid Request")
+    def post(self):
+        args = self.parser.parse_args()
+
+        try:
+            args["filter"] = json.loads(args["filter"])
+        except:
+            abort(400, "Filter must be a valid Python dict")
+
+        try:
+            query_results = serialize(
+                client_db.session.query(eval(args["model"]))
+                    .filter_by(**args["filter"])
+                    .all(),
+            )
+            status_msg, status_code = "OK", 200
+        except (exc.InvalidRequestError, SyntaxError):
+            query_results = None
+            status_msg, status_code = "ERROR", 400
+
+        return {
+            "status": status_msg,
+            "query_results": query_results,
+        },\
+        status_code
+
+
+@api.route("/update")
+class Update(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("model", required=True, type=str, location="form")
+    parser.add_argument("filter", required=True, type=str, location="form")
+    parser.add_argument("field", required=True, type=str, location="form")
+    parser.add_argument("value", required=True, location="form")
+
+    @api.expect(parser)
+    @api.response(200, "Success")
+    @api.response(400, "Invalid Request")
+    def post(self):
+        args = self.parser.parse_args()
+
+        try:
+            args["filter"] = json.loads(args["filter"])
+        except:
+            abort(400, "Filter must be a valid Python dict")
+
+        try:
+            query_result = client_db.session.query(eval(args["model"]))\
+                .filter_by(**args["filter"])\
+                .first()
+
+            if query_result is not None:
+                setattr(query_result, args["field"], args["value"])
+                client_db.session.commit()
+                status_msg, status_code = "OK", 200
+            else:
+                status_msg, status_code = "ERROR", 400
+        except (exc.InvalidRequestError, exc.StatementError, SyntaxError):
+            status_msg, status_code = "ERROR", 400
+
+        return {"status": status_msg}, status_code
+
+
+@api.route("/delete")
+class Delete(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("model", required=True, type=str, location="form")
+    parser.add_argument("filter", required=True, type=str, location="form")
+
+    @api.expect(parser)
+    @api.response(200, "Success")
+    @api.response(400, "Invalid Request")
+    def post(self):
+        args = self.parser.parse_args()
+
+        try:
+            args["filter"] = json.loads(args["filter"])
+        except:
+            abort(400, "Filter must be a valid Python dict")
+
+        try:
+            query_result = client_db.session.query(eval(args["model"]))\
+                .filter_by(**args["filter"])\
+                .first()
+
+            if query_result is not None:
+                client_db.session.delete(query_result)
+                client_db.session.commit()
+                status_msg, status_code = "OK", 200
+            else:
+                status_msg, status_code = "ERROR", 400
+        except (exc.InvalidRequestError, SyntaxError):
+            status_msg, status_code = "ERROR", 400
+
+        return {"status": status_msg}, status_code
+
+
+# Functions
+def serialize(obj):
+    return base64.b64encode(pickle.dumps(obj)).decode("UTF-8")
+
+
+def deserialize(string):
+    return pickle.loads(base64.b64decode(string.encode("UTF-8")))
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=4999)
--- models.py
+++
@@ -1,4 +0,0 @@
-from flask_login import UserMixin
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
