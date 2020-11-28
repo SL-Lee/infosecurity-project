8453744b8753548cbe3068e2af959dbad3d8ad3e
Lim Wen Da <8577@hotmail.sg>
2020-11-28 17:59:43 +0800

    Backup Function

--- app.py
+++ app.py
@@ -28,6 +28,9 @@ import base64
 import json
 import pickle
 import os
+import shutil
+import datetime
+import forms
 
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
@@ -45,6 +48,13 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "danger"
 
+dirname = os.path.dirname(__file__)
+db_location = ""
+# only if backup folder does not exist
+if not os.path.isdir(os.path.join(dirname, "backup")):
+    os.mkdir(os.path.join(dirname, "backup"))
+backup_path = os.path.join(dirname, "backup")
+backup_interval = ""
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -71,6 +81,57 @@ def signup():
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+
+@app.route("/backup", methods=["GET", "POST"])
+def backup():
+    global db_location, backup_interval
+    if db_location == "":
+        form = forms.BackupFirstForm(request.form)
+        if request.method == "POST" and form.validate():
+            db_location = form.source.data
+            backup_datetime = datetime.datetime.now().strftime("Backup %d-%m-%Y %H-%M-%S")
+            os.mkdir(os.path.join(backup_path, backup_datetime))
+            file_backup_path = os.path.join(os.path.join(backup_path, backup_datetime), os.path.basename(db_location))
+
+            backup_interval = {"type": form.interval_type.data, "interval": form.interval.data}
+            shutil.copy2(db_location, file_backup_path)
+
+            return redirect(url_for("index"))
+
+    else:
+        form = forms.BackupForm(request.form)
+        if request.method == "POST" and form.validate():
+            if form.manual.data:
+                # only update, nothing else happens, including changes to settings
+                print("manual backup")
+
+                backup_datetime = datetime.datetime.now().strftime("Backup %d-%m-%Y %H-%M-%S")
+                print(backup_datetime)
+                os.mkdir(os.path.join(backup_path, backup_datetime))
+                file_backup_path = os.path.join(os.path.join(backup_path, backup_datetime), os.path.basename(db_location))
+
+                shutil.copy2(db_location, file_backup_path)
+            elif form.update.data:
+                # will perform a update, and update the settings
+                print("update settings")
+
+                if form.source.data != "" and os.path.isfile(form.source.data):     # if field is not empty and the file is valid
+                    db_location = form.source.data
+                if form.interval.data != "":                                        # if field is not empty
+                    backup_interval = {"type": form.interval_type.data, "interval": form.interval.data}
+
+                backup_datetime = datetime.datetime.now().strftime("Backup %d-%m-%Y %H-%M-%S")
+                os.mkdir(os.path.join(backup_path, backup_datetime))
+                file_backup_path = os.path.join(os.path.join(backup_path, backup_datetime), os.path.basename(db_location))
+
+                shutil.copy2(db_location, file_backup_path)
+            else:
+                # still dk wat to do if this gets executed
+                print("something else happened")
+
+            return redirect(url_for("index"))
+    return render_template("backup.html", form=form)
 
 
 # API routes
