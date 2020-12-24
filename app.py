6ba87b2d85af65fdafe31951f051741490313b26
Charlotte hew <charlottehew14@gmail.com>
2020-12-24 16:26:21 +0800

    File Upload and Download

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ from flask import (
     render_template,
     request,
     url_for,
+    send_file
 )
 from flask_login import (
     current_user,
@@ -33,6 +34,7 @@ from helper_functions import (
     set_config_value,
     validate_api_key,
 )
+from werkzeug.utils import secure_filename
 import datetime
 import forms
 import hashlib
@@ -50,6 +52,8 @@ app.secret_key = os.urandom(16)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///client_db.sqlite3"
 csrf = CSRFProtect(app)
+UPLOAD_FOLDER = 'uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 blueprint = Blueprint("api", __name__, url_prefix="/api")
 authorizations = {
@@ -392,6 +396,41 @@ class Database(Resource):
             )
 
         return {"status": status, "status_msg": status_msg}, status_code
+
+
+# Upload API
+@app.route('/uploadfile', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('no file')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            print('no filename')
+            return redirect(request.url)
+        else:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print("saved file successfully")
+      # send file name as parameter to download
+            return redirect('/downloadfile/'+ filename)
+    return render_template('upload_file.html')
+
+
+# Download API
+@app.route("/downloadfile/<filename>", methods = ['GET'])
+def download_file(filename):
+    return render_template('download.html', value=filename)
+
+
+@app.route('/return-files/<filename>')
+def return_files_tut(filename):
+    file_path = UPLOAD_FOLDER + filename
+    return send_file(file_path, as_attachment=True, attachment_filename='')
 
 
 if __name__ == "__main__":
