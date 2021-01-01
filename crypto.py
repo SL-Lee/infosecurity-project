5633777bd540268573cfcd7b300ae2836192e161
Charlotte hew <charlottehew14@gmail.com>
2021-01-02 01:28:27 +0800

    def encrypt and decrypt

--- crypto.py
+++ crypto.py
@@ -3,58 +3,71 @@ import hashlib
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
-key = get_random_bytes(32)  # Use a stored / generated key
-file_to_encrypt = "client_db.sqlite3"
-buffer_size = 65536  # 64kb
+# key = get_random_bytes(32)  # Use a stored / generated key
+# file_to_encrypt = "client_db.sqlite3"
+# buffer_size = 65536  # 64kb
 
-# === Encrypt ===
 
-# Open the input and output files
-input_file = open(file_to_encrypt, "rb")
-output_file = open(file_to_encrypt + ".encrypted", "wb")
+def encrypt(file_to_encrypt):
+    key = get_random_bytes(32)  # Use a stored / generated key
+    buffer_size = 65536  # 64kb
+    # === Encrypt ===
 
-# Create the cipher object and encrypt the data
-cipher_encrypt = AES.new(key, AES.MODE_CFB)
+    # Open the input and output files
+    input_file = open(file_to_encrypt, "rb")
+    output_file = open(file_to_encrypt + ".encrypted", "wb")
 
-# Initially write the iv to the output file
-output_file.write(cipher_encrypt.iv)
+    # Create the cipher object and encrypt the data
+    cipher_encrypt = AES.new(key, AES.MODE_CFB)
 
-# Keep reading the file into the buffer, encrypting then writing to the new file
-buffer = input_file.read(buffer_size)
+    # Initially write the iv to the output file
+    output_file.write(cipher_encrypt.iv)
 
-while len(buffer) > 0:
-    ciphered_bytes = cipher_encrypt.encrypt(buffer)
-    output_file.write(ciphered_bytes)
+    # Keep reading the file into the buffer, encrypting then writing to the new file
     buffer = input_file.read(buffer_size)
 
-# Close the input and output files
-input_file.close()
-output_file.close()
+    while len(buffer) > 0:
+        ciphered_bytes = cipher_encrypt.encrypt(buffer)
+        output_file.write(ciphered_bytes)
+        buffer = input_file.read(buffer_size)
 
-# === Decrypt ===
+    # Close the input and output files
+    input_file.close()
+    output_file.close()
 
-# Open the input and output files
-input_file = open(file_to_encrypt + ".encrypted", "rb")
-output_file = open(file_to_encrypt + ".decrypted", "wb")
 
-# Read in the iv
-iv = input_file.read(16)
+def decrypt(file_to_encrypt):
+    key = get_random_bytes(32)  # Use a stored / generated key
+    buffer_size = 65536  # 64kb
+    # === Decrypt ===
 
-# Create the cipher object and encrypt the data
-cipher_encrypt = AES.new(key, AES.MODE_CFB, iv=iv)
+    # Open the input and output files
+    input_file = open(file_to_encrypt + ".encrypted", "rb")
+    output_file = open(file_to_encrypt + ".decrypted", "wb")
 
-# Keep reading the file into the buffer, decrypting then writing to the new file
-buffer = input_file.read(buffer_size)
+    # Read in the iv
+    iv = input_file.read(16)
 
-while len(buffer) > 0:
-    decrypted_bytes = cipher_encrypt.decrypt(buffer)
-    output_file.write(decrypted_bytes)
+    # Create the cipher object and encrypt the data
+    cipher_encrypt = AES.new(key, AES.MODE_CFB, iv=iv)
+
+    # Keep reading the file into the buffer, decrypting then writing to the new file
     buffer = input_file.read(buffer_size)
 
-# Close the input and output files
-input_file.close()
-output_file.close()
+    while len(buffer) > 0:
+        decrypted_bytes = cipher_encrypt.decrypt(buffer)
+        output_file.write(decrypted_bytes)
+        buffer = input_file.read(buffer_size)
 
+    # Close the input and output files
+    input_file.close()
+    output_file.close()
+
+
+file = input("Enter file to upload: ")
+encrypt(file)
+decrypt(file)
+"""
 # === Proving the data matches (hash the files and compare the hashes) ===
 
 
@@ -75,3 +88,4 @@ def get_file_hash(file_path):
 assert get_file_hash(file_to_encrypt) == get_file_hash(
     file_to_encrypt + ".decrypted"
 ), "Files are not identical"
+"""
