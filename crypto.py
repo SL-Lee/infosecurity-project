b249574fe2ca8517a28e78534bd930ff073ffa1a
Charlotte hew <charlottehew14@gmail.com>
2021-01-03 20:11:06 +0800

    updated change to crypto file

--- crypto.py
+++ crypto.py
@@ -16,29 +16,29 @@ def encrypt(message, key, key_size=256):
 
 
 def decrypt(ciphertext, key):
-    iv = ciphertext[:AES.block_size]
+    iv = ciphertext[: AES.block_size]
     cipher = AES.new(key, AES.MODE_CBC, iv)
-    plaintext = cipher.decrypt(ciphertext[AES.block_size:])
+    plaintext = cipher.decrypt(ciphertext[AES.block_size :])
     return plaintext.rstrip(b"\0")
 
 
 def encrypt_file(file_name, key):
-    with open(file_name, 'rb') as fo:
+    with open(file_name, "rb") as fo:
         plaintext = fo.read()
     enc = encrypt(plaintext, key)
-    with open(file_name + ".enc", 'wb') as fo:
+    with open(file_name + ".enc", "wb") as fo:
         fo.write(enc)
 
 
 def decrypt_file(file_name, key):
-    with open(file_name, 'rb') as fo:
+    with open(file_name, "rb") as fo:
         ciphertext = fo.read()
     dec = decrypt(ciphertext, key)
-    with open(file_name[:-4] + ".dec", 'wb') as fo:
+    with open(file_name[:-4] + ".dec", "wb") as fo:
         fo.write(dec)
 
 
-key = b'\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18'
+key = b"\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18"
 
-encrypt_file('client_db.sqlite3', key)
-decrypt_file('client_db.sqlite3.enc', key)
+encrypt_file("client_db.sqlite3", key)
+decrypt_file("client_db.sqlite3.enc", key)
