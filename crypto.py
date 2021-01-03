319b1c538ae73059c3da3e3c218b8428b2923de3
Charlotte hew <charlottehew14@gmail.com>
2021-01-03 20:10:07 +0800

    Changed Encrypt and Decrypt File Code

--- crypto.py
+++ crypto.py
@@ -1,91 +1,44 @@
-import hashlib
+#!/usr/bin/python3
 
+from Crypto import Random
 from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-
-# key = get_random_bytes(32)  # Use a stored / generated key
-# file_to_encrypt = "client_db.sqlite3"
-# buffer_size = 65536  # 64kb
 
 
-def encrypt(file_to_encrypt):
-    key = get_random_bytes(32)  # Use a stored / generated key
-    buffer_size = 65536  # 64kb
-    # === Encrypt ===
-
-    # Open the input and output files
-    input_file = open(file_to_encrypt, "rb")
-    output_file = open(file_to_encrypt + ".encrypted", "wb")
-
-    # Create the cipher object and encrypt the data
-    cipher_encrypt = AES.new(key, AES.MODE_CFB)
-
-    # Initially write the iv to the output file
-    output_file.write(cipher_encrypt.iv)
-
-    # Keep reading the file into the buffer, encrypting then writing to the new file
-    buffer = input_file.read(buffer_size)
-
-    while len(buffer) > 0:
-        ciphered_bytes = cipher_encrypt.encrypt(buffer)
-        output_file.write(ciphered_bytes)
-        buffer = input_file.read(buffer_size)
-
-    # Close the input and output files
-    input_file.close()
-    output_file.close()
+def pad(s):
+    return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
 
 
-def decrypt(file_to_encrypt):
-    key = get_random_bytes(32)  # Use a stored / generated key
-    buffer_size = 65536  # 64kb
-    # === Decrypt ===
-
-    # Open the input and output files
-    input_file = open(file_to_encrypt + ".encrypted", "rb")
-    output_file = open(file_to_encrypt + ".decrypted", "wb")
-
-    # Read in the iv
-    iv = input_file.read(16)
-
-    # Create the cipher object and encrypt the data
-    cipher_encrypt = AES.new(key, AES.MODE_CFB, iv=iv)
-
-    # Keep reading the file into the buffer, decrypting then writing to the new file
-    buffer = input_file.read(buffer_size)
-
-    while len(buffer) > 0:
-        decrypted_bytes = cipher_encrypt.decrypt(buffer)
-        output_file.write(decrypted_bytes)
-        buffer = input_file.read(buffer_size)
-
-    # Close the input and output files
-    input_file.close()
-    output_file.close()
+def encrypt(message, key, key_size=256):
+    message = pad(message)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return iv + cipher.encrypt(message)
 
 
-file = input("Enter file to upload: ")
-encrypt(file)
-decrypt(file)
-"""
-# === Proving the data matches (hash the files and compare the hashes) ===
+def decrypt(ciphertext, key):
+    iv = ciphertext[:AES.block_size]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = cipher.decrypt(ciphertext[AES.block_size:])
+    return plaintext.rstrip(b"\0")
 
 
-def get_file_hash(file_path):
-    block_size = 65536
-    file_hash = hashlib.sha256()
-
-    with open(file_path, "rb") as f:
-        fb = f.read(block_size)
-
-        while len(fb) > 0:
-            file_hash.update(fb)
-            fb = f.read(block_size)
-
-    return file_hash.hexdigest()
+def encrypt_file(file_name, key):
+    with open(file_name, 'rb') as fo:
+        plaintext = fo.read()
+    enc = encrypt(plaintext, key)
+    with open(file_name + ".enc", 'wb') as fo:
+        fo.write(enc)
 
 
-assert get_file_hash(file_to_encrypt) == get_file_hash(
-    file_to_encrypt + ".decrypted"
-), "Files are not identical"
-"""
+def decrypt_file(file_name, key):
+    with open(file_name, 'rb') as fo:
+        ciphertext = fo.read()
+    dec = decrypt(ciphertext, key)
+    with open(file_name[:-4] + ".dec", 'wb') as fo:
+        fo.write(dec)
+
+
+key = b'\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18'
+
+encrypt_file('client_db.sqlite3', key)
+decrypt_file('client_db.sqlite3.enc', key)
