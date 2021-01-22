ff63588321b136888bd77ecd19f13df47829d4a9
Charlotte hew <charlottehew14@gmail.com>
2021-01-22 13:37:27 +0800

    Removed redundant code

--- app.py
+++ app.py
@@ -1077,48 +1077,9 @@ def upload_file():
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
 
-        def pad(string):
-            return string + b"\0" * (
-                AES.block_size - len(string) % AES.block_size
-            )
-
-        def encrypt(message, key):
-            message = pad(message)
-            iv = Random.new().read(AES.block_size)
-            cipher = AES.new(key, AES.MODE_CBC, iv)
-            return iv + cipher.encrypt(message)
-
-        def decrypt(ciphertext, key):
-            iv = ciphertext[: AES.block_size]
-            cipher = AES.new(key, AES.MODE_CBC, iv)
-            plaintext = cipher.decrypt(ciphertext[AES.block_size :])
-            return plaintext.rstrip(b"\0")
-
-        def encrypt_file(file_name, key):
-            with open(file_name, "rb") as file:
-                plaintext = file.read()
-
-            enc = encrypt(plaintext, key)
-
-            with open(file_name + ".enc", "wb") as file:
-                file.write(enc)
-
-        def decrypt_file(file_name, key):
-            with open(file_name, "rb") as file:
-                ciphertext = file.read()
-
-            dec = decrypt(ciphertext, key)
-
-            with open(file_name[:-4] + ".dec", "wb") as file:
-                file.write(dec)
-
-        key = (
-            b"\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e"
-            b"[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18"
-        )
         if "encrypt" in request.form:
             encrypt_file(
-                os.path.join(app.config["UPLOAD_FOLDER"], filename), key
+                os.path.join(app.config["UPLOAD_FOLDER"], filename), KEY
             )
             print("saved file successfully")
             # send file name as parameter to download
@@ -1126,7 +1087,7 @@ def upload_file():
 
         if "decrypt" in request.form:
             decrypt_file(
-                os.path.join(app.config["UPLOAD_FOLDER"], filename), key
+                os.path.join(app.config["UPLOAD_FOLDER"], filename), KEY
             )
             print("saved file2 successfully")
             # send file name as parameter to download
