851da4e7cde4aedc9f4dfa5f7057de5fc0693b7e
SL-Lee <45960387+SL-Lee@users.noreply.github.com>
2021-02-01 23:39:43 +0800

    Refactor main application into individual blueprints

---
+++ blueprints/sensitive_fields_blueprint.py
@@ -0,0 +1,74 @@
+from flask import Blueprint, redirect, render_template, request, url_for
+
+import forms
+from helper_functions import required_permissions
+from server_models import Rule, server_db
+
+sensitive_fields_blueprint = Blueprint("sensitive_fields", __name__)
+
+
+@sensitive_fields_blueprint.route("/sensitive-fields", methods=["GET"])
+@required_permissions("manage_sensitive_fields")
+def get_sensitive_fields():
+    sensitive_fields = Rule.query.all()
+    return render_template(
+        "sensitive-fields.html", sensitive_fields=sensitive_fields
+    )
+
+
+@sensitive_fields_blueprint.route(
+    "/sensitive-fields/add", methods=["GET", "POST"]
+)
+@required_permissions("manage_sensitive_fields")
+def add_sensitive_fields():
+    form = forms.SensitiveFieldForm(request.form)
+
+    if request.method == "POST" and form.validate():
+        rule = Rule(
+            contents=form.sensitive_field.data,
+            action=form.action.data,
+            alert_level=form.alert_level.data,
+            occurrence_threshold=form.occurrence_threshold.data,
+        )
+        server_db.session.add(rule)
+        server_db.session.commit()
+
+        return redirect(url_for(".get_sensitive_fields"))
+
+    return render_template("sensitive-fields-add.html", form=form)
+
+
+@sensitive_fields_blueprint.route(
+    "/sensitive-fields/update/<field>", methods=["GET", "POST"]
+)
+@required_permissions("manage_sensitive_fields")
+def update_sensitive_fields(field):
+    form = forms.SensitiveFieldForm(request.form)
+    rule = Rule.query.filter_by(id=field).first_or_404()
+
+    if request.method == "POST" and form.validate():
+        rule.contents = form.sensitive_field.data
+        rule.action = form.action.data
+        rule.alert_level = form.alert_level.data
+        rule.occurrence_threshold = form.occurrence_threshold.data
+        server_db.session.commit()
+
+        return redirect(url_for(".get_sensitive_fields"))
+
+    form.sensitive_field.data = rule.contents
+    form.action.data = rule.action
+    form.occurrence_threshold.data = rule.occurrence_threshold
+
+    return render_template("sensitive-fields-update.html", form=form, rule=rule)
+
+
+@sensitive_fields_blueprint.route(
+    "/sensitive-fields/delete/<field>", methods=["GET", "POST"]
+)
+@required_permissions("manage_sensitive_fields")
+def delete_sensitive_fields(field):
+    rule = Rule.query.filter_by(id=field).first_or_404()
+    server_db.session.delete(rule)
+    server_db.session.commit()
+
+    return redirect(url_for(".get_sensitive_fields"))
---
+++ blueprints/user_management_blueprint.py
@@ -0,0 +1,198 @@
+import datetime
+import hashlib
+import os
+
+import sqlalchemy
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import current_user
+
+import forms
+from helper_functions import required_permissions
+from server_models import ServerPermission, ServerUser, server_db
+
+user_management_blueprint = Blueprint("user_management", __name__)
+
+
+@user_management_blueprint.route("/user-management")
+@required_permissions("manage_users")
+def user_management():
+    server_users = ServerUser.query.all()
+    return render_template("user-management.html", server_users=server_users)
+
+
+@user_management_blueprint.route(
+    "/user-management/create", methods=["GET", "POST"]
+)
+@required_permissions("manage_users")
+def user_management_create():
+    create_user_form = forms.CreateUserForm(request.form)
+    create_user_form.permissions.choices = [
+        (server_permission.name, server_permission.name)
+        for server_permission in ServerPermission.query.all()
+    ]
+
+    if request.method == "POST" and create_user_form.validate():
+        new_server_user_password_salt = os.urandom(32)
+        new_server_user_password_hash = hashlib.scrypt(
+            password=create_user_form.password.data.encode("UTF-8"),
+            salt=new_server_user_password_salt,
+            n=32768,
+            r=8,
+            p=1,
+            maxmem=33816576,
+        )
+        new_server_user = ServerUser(
+            username=create_user_form.username.data,
+            password_salt=new_server_user_password_salt,
+            password_hash=new_server_user_password_hash,
+            date_created=datetime.datetime.now(),
+        )
+
+        for server_permission in create_user_form.permissions.data:
+            new_server_user_permission = ServerPermission.query.get(
+                server_permission
+            )
+
+            if new_server_user_permission is not None:
+                new_server_user.permissions.append(new_server_user_permission)
+
+        try:
+            server_db.session.add(new_server_user)
+            server_db.session.commit()
+            flash("New user created successfully.", "success")
+        except sqlalchemy.exc.IntegrityError:
+            flash(
+                "Another user account already has the username of '"
+                f"{create_user_form.username.data}'. Please try again with a "
+                "unique username.",
+                "danger",
+            )
+            return redirect(url_for(".user_management_create"))
+
+        return redirect(url_for(".user_management"))
+
+    return render_template(
+        "user-management-form.html",
+        title="User Management — Create User",
+        form=create_user_form,
+        form_action_route=url_for(".user_management_create"),
+        action_name="Create",
+    )
+
+
+@user_management_blueprint.route(
+    "/user-management/edit/<int:server_user_id>", methods=["GET", "POST"]
+)
+@required_permissions("manage_users")
+def user_management_edit(server_user_id):
+    edit_user_form = forms.CreateUserForm(request.form)
+    edit_user_form.permissions.choices = [
+        (server_permission.name, server_permission.name)
+        for server_permission in ServerPermission.query.all()
+    ]
+    server_user = ServerUser.query.get(server_user_id)
+
+    if request.method == "POST" and edit_user_form.validate():
+        server_user.username = edit_user_form.username.data
+        password_salt = os.urandom(32)
+        password_hash = hashlib.scrypt(
+            password=edit_user_form.password.data.encode("UTF-8"),
+            salt=password_salt,
+            n=32768,
+            r=8,
+            p=1,
+            maxmem=33816576,
+        )
+        server_user.password_salt = password_salt
+        server_user.password_hash = password_hash
+
+        if (
+            ServerUser.query.count() == 1
+            and "manage_users" not in edit_user_form.permissions.data
+        ):
+            flash(
+                "You cannot unassign the 'manage_users' permission from the "
+                "only user that has this permission.",
+                "danger",
+            )
+            return redirect(
+                url_for(".user_management_edit", server_user_id=server_user_id)
+            )
+
+        if (
+            server_user_id == current_user.id
+            and "manage_users" not in edit_user_form.permissions.data
+        ):
+            flash(
+                "You cannot unassign the 'manage_users' permission from the "
+                "current user.",
+                "danger",
+            )
+            return redirect(
+                url_for(".user_management_edit", server_user_id=server_user_id)
+            )
+
+        server_user.permissions = []
+
+        for server_permission in edit_user_form.permissions.data:
+            server_user_permission = ServerPermission.query.get(
+                server_permission
+            )
+
+            if server_user_permission is not None:
+                server_user.permissions.append(server_user_permission)
+
+        try:
+            server_db.session.commit()
+            flash("User edited successfully.", "success")
+        except sqlalchemy.exc.IntegrityError:
+            flash(
+                "Another user account already has the username of '"
+                f"{edit_user_form.username.data}'. Please try again with a "
+                "unique username.",
+                "danger",
+            )
+            return redirect(
+                url_for(".user_management_edit", server_user_id=server_user_id)
+            )
+
+        return redirect(url_for(".user_management"))
+
+    edit_user_form.username.data = server_user.username
+    edit_user_form.permissions.data = [
+        server_permission.name for server_permission in server_user.permissions
+    ]
+
+    return render_template(
+        "user-management-form.html",
+        title="User Management — Edit User",
+        form=edit_user_form,
+        form_action_route=url_for(
+            ".user_management_edit", server_user_id=server_user_id
+        ),
+        action_name="Edit",
+    )
+
+
+@user_management_blueprint.route("/user-management/delete", methods=["POST"])
+@required_permissions("manage_users")
+def user_management_delete():
+    if ServerUser.query.count() == 1:
+        flash("You cannot delete the only remaining user.", "danger")
+        return redirect(url_for(".user_management"))
+
+    try:
+        server_user_id = int(request.form["server-user-id"])
+
+        if server_user_id == current_user.id:
+            flash("You cannot delete the current user.", "danger")
+            return redirect(url_for(".user_management"))
+
+        server_user = ServerUser.query.get(server_user_id)
+        server_db.session.delete(server_user)
+        server_db.session.commit()
+        flash("User deleted successfully.", "success")
+    except:
+        flash("There was an error while deleting the user.", "danger")
+
+    return redirect(url_for(".user_management"))
